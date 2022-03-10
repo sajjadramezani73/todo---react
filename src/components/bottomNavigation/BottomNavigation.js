02c438ece329ba76be9dc7b4cb4399ccf8b0a4ef
sajjadramezani73 <sajjad.payam77@gmail.com>
2022-03-10 23:01:42 +0330

    edit style bottom navigation

--- src/components/bottomNavigation/BottomNavigation.js
+++ src/components/bottomNavigation/BottomNavigation.js
@@ -2,13 +2,12 @@ import LoadIconSvg from '../../util/LoadSvgIcon'
 
 const BottomNavigation = ({ clicked }) => {
     return (
-        <div className='bg-blue h-10 relative bottomNavigation'>
+        <div className='bg-blue h-14 relative bottomNavigation'>
             <div
-                className="flex justify-center items-center absolute left-1/2 bottom-1/3 w-14 h-14 bg-blue rounded-full border-[6px] border-silver cursor-pointer clearHighLight btnPlus"
+                className="flex justify-center items-center absolute left-1/2 bottom-1/2 w-14 h-14 bg-blue rounded-full border-8 border-silver cursor-pointer clearHighLight btnPlus"
                 onClick={clicked}>
                 <LoadIconSvg name="plus" size="35" color='#ffffff' />
             </div>
-            <div className="div"></div>
         </div>
     )
 }
